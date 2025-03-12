020ab50c69b847114683c1b5587b240ca70cef36
Tariqkhan61 <161859910+Tariqkhan61@users.noreply.github.com>
2025-03-12 05:17:42 +0500

    Add files via upload

---
+++ ke.py
@@ -0,0 +1,9 @@
+number = int(input("Enter any current: "))
+
+if number == 1:
+    print("K Electric On")
+elif number == 0:
+    print("UPS On")
+else:
+    print("Wrong value. Enter correct current: only 1 and 0 will be accepted.")
+
